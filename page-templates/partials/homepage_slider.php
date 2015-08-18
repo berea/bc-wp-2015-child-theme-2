6eaddc53997d7ce1c89c28c92867ad7ded8ebee6
Ron Northrip <ron.northrip@gmail.com>
2015-08-17 20:37:59 -0400

    Implement changes for departmental header partial

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -1,4 +1,5 @@
 <!-- Child 2 homepage slider -->
-<div class="homepage-slider homepage-slider-sizing-dept">
+<div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
+	<div class="homepage-slider-background"></div>
     <h1><?php echo get_bloginfo('name'); ?></h1>
 </div>
