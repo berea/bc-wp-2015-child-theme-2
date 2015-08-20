4ff7c31a4da08ede6056b3538af300070fe39234
Ron Northrip <ron.northrip@gmail.com>
2015-08-20 05:13:36 -0400

    Implement slider-textblock in child them 2

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -1,5 +1,8 @@
 <!-- Child 2 homepage slider -->
 <div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
 	<div class="homepage-slider-background"></div>
-    <h1><?php echo get_bloginfo('name'); ?></h1>
+	<div class="slider-textblock">
+		<div class="slider-breadcrumbs">Bread &nbsp;/&nbsp; Crumbs &nbsp;/&nbsp; Here</div>
+    	<h1 style=""><?php echo get_bloginfo('name'); ?></h1>
+    </div>
 </div>
