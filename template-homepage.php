ece07142fc041445e8c09568bf52988831aef440
Ron Northrip <ron.northrip@gmail.com>
2015-08-26 05:48:48 -0400

    Removing main-footer-shim globally

--- template-homepage.php
+++ template-homepage.php
@@ -61,9 +61,6 @@ get_header(); ?>
 
                 </main><!-- #main -->
                 
-                <!-- site-main-footer-shim -->
-                <div id="main-footer-shim" class="site-main-footer-shim"></div>
-                
         </div><!-- #primary -->
 
 <?php get_footer(); ?>
