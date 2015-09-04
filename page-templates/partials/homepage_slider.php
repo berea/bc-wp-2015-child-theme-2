9ed7a0c6a5880cf90c8938b2beb2d8e676a9a8a3
Janine Hempy <jhempy@gmail.com>
2015-09-04 11:18:21 -0400

    Incremental save so that Skaught can continue my work on the breadcrumbs.

--- page-templates/partials/homepage_slider.php
+++ page-templates/partials/homepage_slider.php
@@ -2,7 +2,9 @@
 <div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
 	<div class="homepage-slider-background"></div>
 	<div class="slider-textblock">
-		<div class="slider-breadcrumbs">Bread &nbsp;/&nbsp; Crumbs &nbsp;/&nbsp; Here</div>
-    	<h1 style=""><?php echo get_bloginfo('name'); ?></h1>
-    </div>
+		<div class="slider-breadcrumbs"><?php if ( function_exists('yoast_breadcrumb') ) {
+				yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+			} ?></div>
+		<h1 style=""><?php echo get_bloginfo('name'); ?></h1>
+	</div>
 </div>
---
+++ page-templates/partials/secondary_slider.php
@@ -0,0 +1,10 @@
+<!-- Child 2 secondary page slider -->
+<div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
+    <div class="secondary-slider-background"></div>
+    <div class="slider-textblock">
+        <div class="slider-breadcrumbs"><?php if ( function_exists('yoast_breadcrumb') ) {
+                yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+            } ?></div>
+        <h1 style=""><?php echo get_bloginfo('name'); ?></h1>
+    </div>
+</div>
