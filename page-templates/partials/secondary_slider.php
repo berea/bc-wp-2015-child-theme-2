e08cb30fafce0fb79d3e4d8d8c8bbb1576d5a7f7
Dan Adams <danadams@Dans-MacBook-Pro-2.local>
2017-08-30 18:45:17 -0400

    adding is_page check for giving day page

--- page-templates/partials/secondary_slider.php
+++ page-templates/partials/secondary_slider.php
@@ -1,5 +1,18 @@
 <!-- Child 2 secondary page slider -->
+<?php
+if(is_page(970))
+{
+?>
+<div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192_givingday ">
+<?php
+}
+else
+{
+?>
 <div class="homepage-slider homepage-slider-sizing-dept homepage-slider-sizing-aspect-ratio ratio_1024x192 ">
+<?php
+}
+?>
     <div class="secondary-slider-background"></div>
     <div class="slider-textblock">
         <div class="slider-breadcrumbs"><?php if ( function_exists('yoast_breadcrumb') ) {
